97b3c0aaac075ad314d2cde90453d853bcf4cd69
imkaranverma <106688614+imkaranverma@users.noreply.github.com>
2024-05-30 01:28:13 +0530

    cors 4.0

--- frontend/src/components/AppLayout.js
+++ frontend/src/components/AppLayout.js
@@ -17,7 +17,7 @@ const AppLayout = ({ children }) => {
                 </div>
             </div>
             <footer className=' w-[90vw]  m-auto text-center p-6'>
-                <p className='text-base text-lg'>Made with &#x2764;&#xfe0f; by <a className='text-[#6366F1]' href='https://iamkaranverma.netlify.app/'>KARAN VERMA</a></p>
+                <p className='text-lg'>Made with &#x2764;&#xfe0f; by <a className='text-[#6366F1]' href='https://iamkaranverma.netlify.app/'>KARAN VERMA</a></p>
             </footer>
         </div>
     )
